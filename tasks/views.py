351c4297ec9e8e78ba0ae2e3a0cf285de2c1a6bf
mike-droid <miguelreyesmoreno@hotmail.com>
2023-06-22 12:16:13 -0600

    logout

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.db import IntegrityError
 
 # Create your views here.
@@ -38,3 +38,8 @@ def signup(request):
 
 def tasks(request):
   return render(request, 'tasks.html')
+
+
+def signout(request):
+  logout(request)
+  return redirect('home')

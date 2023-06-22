d0ae3bef51b29a03ec1383a8603b8999d5dab9eb
mike-droid <miguelreyesmoreno@hotmail.com>
2023-06-22 11:18:05 -0600

    registro de usuarios

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,6 +20,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello_world),
-    path('signup/', views.hello_world)
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
 
-def hello_world(request):
-  return render(request, 'signup.html', {
-    'user_form': UserCreationForm()
-  })
+def home(request):
+  return render(request, 'home.html')
+
+
+def signup(request):
+  if request.method == 'GET':
+    return render(request, 'signup.html', {
+      'user_form': UserCreationForm()
+    })
+  else:
+    if request.POST['password1'] == request.POST['password2']:
+      try:
+        user = User.objects.create_user(
+          username=request.POST['username'],
+          password=request.POST['password1']
+        )
+        user.save()
+        return HttpResponse('User created successfully')
+      except:
+        return HttpResponse('Username already exists')
+    return HttpResponse('Passwords do not match')

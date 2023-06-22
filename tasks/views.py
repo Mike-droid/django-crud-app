12558188d63f3d738efdd98808f746cff5a1431a
mike-droid <miguelreyesmoreno@hotmail.com>
2023-06-22 11:55:18 -0600

    Login y vista de tareas

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -22,8 +23,9 @@ def signup(request):
           password=request.POST['password1']
         )
         user.save()
-        return HttpResponse('User created successfully')
-      except:
+        login(request, user)
+        return redirect('tasks')
+      except IntegrityError:
         return render(request, 'signup.html', {
           'user_form': UserCreationForm(),
           'error': 'Username already exists'
@@ -32,3 +34,7 @@ def signup(request):
       'user_form': UserCreationForm(),
       'error': 'Passwords do not match'
     })
+
+
+def tasks(request):
+  return render(request, 'tasks.html')

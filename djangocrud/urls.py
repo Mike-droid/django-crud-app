7b41755d8d035c06a1a53bf561ca53525585d5a4
mike-droid <miguelreyesmoreno@hotmail.com>
2023-06-22 10:58:55 -0600

    formulario signup

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,5 +20,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello_world)
+    path('', views.hello_world),
+    path('signup/', views.hello_world)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def hello_world(request):
-  return HttpResponse('hello world')
+  return render(request, 'signup.html', {
+    'user_form': UserCreationForm()
+  })

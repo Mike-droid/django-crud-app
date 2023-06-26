16069d3032442e074fb7d204ca615466caa3a140
mike-droid <miguelreyesmoreno@hotmail.com>
2023-06-26 13:09:32 -0600

    Estilos con bootstrap

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class CreateTaskForm(ModelForm):
+class CreateTaskForm(forms.ModelForm):
   class Meta:
     model = Task
     fields = ['title', 'description', 'important']
+    widgets = {
+      'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write a title'}),
+      'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a description'}),
+      'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'}),
+    }
